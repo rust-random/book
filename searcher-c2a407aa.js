ecb27e8616edda863c0dd90dc31e674e21ff34a8
dhardy <dhardy@users.noreply.github.com>
2026-02-11 12:26:24 +0000

    deploy: 93bc6cf33f1280fd071021f0d24e2b10d524fe29

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-36cf1434.js',
+                path_to_root + 'searchindex-753e24b7.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
